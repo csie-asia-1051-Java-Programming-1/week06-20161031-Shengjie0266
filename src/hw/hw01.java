cbd5c9b5db615f79ea0d7e28491b8f312816d2a3
Shengjie0266 <105021040@live.asia.edu.tw>
2016-11-06 19:37:13 +0800

    HW
    HW

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,23 @@
 package hw;
-
+import java.util.*;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		System.out.print("請輸入一個溫度值"+ ","+"計算方式");
+		float n=scn.nextFloat();
+		int m=scn.nextInt();
+		fun(n,m);
+		
 	}
-
+public static void fun(float n,int m){
+	if(m==1){
+		n=(n-32)*((float)5/9);
+	}
+	if(m==2){
+		n=n*((float)9/5)+32;
+	}
+	System.out.print(n);
+}
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,22 @@
 package hw;
-
+import java.util.*;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		System.out.print("請輸入性別,身高");
+		int n=scn.nextInt(); //性別
+		double m=scn.nextFloat(); //身高
+		fun(n,m);
 	}
-
+public static void fun(int n,double m){
+	if(n==1){
+		m=(m-170)*0.6+62;
+	}
+	if(n==2){
+		m=(m-158)*0.5+52;
+	}
+	System.out.print(m);
+}
 }
